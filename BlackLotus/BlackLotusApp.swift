b700e4351a4ddf9811773bb77eed886c84765b77
Norlan Tibanear <ntibanear@gmail.com>
2021-12-26 16:29:57 -0500

    Initial Commit

---
+++ BlackLotus/BlackLotusApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackLotusApp.swift
+//  BlackLotus
+//
+//  Created by Norlan Tibanear on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct BlackLotusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
